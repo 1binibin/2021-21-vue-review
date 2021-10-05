77f49bbd7538f63725f335948f3e2249f0122ee1
1binibin <namwonbin@gmail.com>
2021-10-05 16:59:17 +0900

    first commit  - 갤러리 완성

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-wrapper">
+    <ImgCp :src="src" :title="title" />
+    <ul class="thumb-wrap">
+      <ThumbCp v-for="v in foods" :key="v.id" :food="v" @@change="onChange"/>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+import ImgCp from './components/ImgCp.vue';
+import ThumbCp from './components/ThumbCp.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      foods: [],
+      title: '',
+      src: '',
+    };
+  },
+  components: { ImgCp, ThumbCp },
+  methods: {
+    onChange({ title, src }) {
+      this.title = title;
+      this.src = src;
+    },
+  },
+  async created() {
+    const { data } = await axios.get('/json/foods.json');
+    this.foods = data;
+    this.title = this.foods[0].title;
+    this.src = this.foods[0].src;
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app-wrapper {
+  max-width: $wrapper-width;
+  margin: 1em auto;
+  .thumb-wrap {
+    margin: .5em 0;
+    border: 1px solid $secondary-color;
+    padding: .325em;
+    @include flex($jc: space-between);
+  }
 }
 </style>
---
+++ src/components/ThumbCp.vue
@@ -0,0 +1,32 @@
+<template>
+  <li class="thumb">
+    <img :src="food.src" :alt="food.title" class="w-100" @click="onChange">
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TumbCp',
+  props: ['food'],
+  methods: {
+    onChange(e) {
+      this.$emit('@change', { src: e.target.src, alt: e.target.title });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.thumb {
+  cursor: pointer;
+  margin: 1%;
+  background-color: $light-color;
+  img {
+    opacity: .5;
+    transition: opacity .75s;
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+</style>
